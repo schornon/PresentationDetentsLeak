9489752569b01d450e6b0669a39ae9d30a4cbb96
Chornonoh Serhii <cs.hilfy@gmail.com>
2023-09-06 10:13:39 +0300

    Leak reproduced

--- PresentationDetentsLeak/ContentView.swift
+++ PresentationDetentsLeak/ContentView.swift
@@ -7,20 +7,58 @@
 
 import SwiftUI
 
+class ContentViewModel: ObservableObject {
+    @Published var detailsPresented: Bool = false
+    var detailsVM: DetailsViewModel?
+    
+    func showDetails() {
+        detailsVM = .init()
+        detailsPresented.toggle()
+    }
+}
+
 struct ContentView: View {
+    @StateObject var vm = ContentViewModel()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Color.blue
+            .onTapGesture {
+                vm.showDetails()
+            }
+            .sheet(isPresented: $vm.detailsPresented) {
+                DetailsView()
+                    .environmentObject(vm.detailsVM!)
+                    .edgesIgnoringSafeArea(.bottom)
+                    .onDisappear {
+                        vm.detailsVM = nil
+                    }
+                    .presentationDetents([.medium, .large]) // DetailsViewModel won't deinit
+//                    .presentationDetents([.medium]) // DetailsViewModel will deinit as expected
+            }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+class DetailsViewModel: ObservableObject {
+    init() {
+        print("DetailsVM init")
+    }
+    deinit {
+        print("DetailsVM deinit")
+    }
+}
+
+struct DetailsView: View {
+    @EnvironmentObject var vm: DetailsViewModel
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        Color.yellow
+            .onTapGesture {
+                dismiss()
+            }
     }
 }
