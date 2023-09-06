6ee92236d3162ad9373517167f5aa4f4b811400b
Chornonoh Serhii <cs.hilfy@gmail.com>
2023-09-06 09:54:11 +0300

    Initial Commit

---
+++ PresentationDetentsLeak/PresentationDetentsLeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationDetentsLeakApp.swift
+//  PresentationDetentsLeak
+//
+//  Created by mbp2 hilfy on 06.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct PresentationDetentsLeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
